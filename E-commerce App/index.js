7c063726d44f739ce7721b21d411e10ee3f59c13
IkramCode <moikramcoder@gmail.com>
2024-07-11 20:58:59 +0500

    Added routing

---
+++ E-commerce App/index.js
@@ -0,0 +1,7 @@
+import Header from "./src/Components/Header/Header";
+import Footer from "./src/Components/Footer/Footer"
+import Layout from './Layout'
+import Home from './src/Pages/Home/Home'
+import Cart from './src/Pages/Cart/Cart'
+
+export { Header , Footer , Home , Cart , Layout}
--- E-commerce App/src/main.jsx
+++ E-commerce App/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {  createBrowserRouter, createRoutesFromElements, RouterProvider , Route} from "react-router-dom";
+import {Home , Cart , Layout} from '../index.js';
 
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Home />} />
+      <Route path='/cart' element={<Cart />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
