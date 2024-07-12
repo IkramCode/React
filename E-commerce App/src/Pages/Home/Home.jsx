8161185c01c13eb1d7f81f2571408012b199a1b7
IkramCode <moikramcoder@gmail.com>
2024-07-12 22:27:24 +0500

    some tailwind changes

--- E-commerce App/src/Pages/Home/Home.jsx
+++ E-commerce App/src/Pages/Home/Home.jsx
@@ -43,11 +43,20 @@ export default function Home() {
       {error && <div>Error: {error.message}</div>}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
-          <div key={product.id} className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300">
-            <img src={product.image} alt={product.title} className="w-full h-48 object-contain p-4" />
+          <div
+            key={product.id}
+            className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-48 object-contain p-4"
+            />
             <div className="p-4">
               <p className="text-lg font-semibold mb-2">{product.title}</p>
-              <p className="font-semibold text-gray-800">Price: ${product.price}</p>
+              <p className="font-semibold text-gray-800">
+                Price: ${product.price}
+              </p>
               <button
                 onClick={() =>
                   cartItems.some((item) => item.item.id === product.id)
